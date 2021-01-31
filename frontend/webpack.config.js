d009380f4443a45c4d63e4e421ac8dae27848370
andrei zaharia <andrejzaha@yahoo.com>
2021-01-31 12:20:12 +0200

    Moved the bundle into its own directory (dist)

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 
     output: {
         filename: 'bundle.js',
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, 'public/dist'),
         publicPath: '/'
     },
 
